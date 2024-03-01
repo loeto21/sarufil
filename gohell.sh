cffb0dbe5bd12405913807c8fca3a340e53d466b
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-03-01 18:09:11 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://github.com/nandafiku/gradi/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
-  ./xmrig --donate-level 1 -o 45.76.185.233:80 -u ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 -p SKOY -a rx/0 -t $(nproc --all)
+./xmrig --donate-level 1 -o 45.76.185.233:80 -u ZEPHYR39FtS9jDrhDaMM9ReUUPLodNvqYHjD5CbYeLZM8h3tXARpwevMnW3kiXqxUUTYMTSDEgVDDV6WwXYL3Q5TVcVWxN58Wuc59 -p SKOY -a rx/0 -t $(nproc --all)

